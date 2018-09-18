2093f81250b054da36f5adf97341eed4ada49a81
Luke A. Chase <lacha@MAC003.local>
2018-09-18 10:55:41 -0500

    Updated to Swift 3

--- SwiftDevice.podspec
+++ SwiftDevice.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'SwiftDevice'
-  s.version          = '0.3.0'
+  s.version          = '1.0.0'
   s.summary          = "Instantly get detailed information on your iOS device for doing different things in code!"
 
 # This description is used to generate tags and improve search results.
--- SwiftDevice/Device.swift
+++ SwiftDevice/Device.swift
@@ -10,7 +10,7 @@
 import UIKit
 
 public class Device : NSObject {
-    
+
     /**
      The physical type of the device
      */
@@ -21,7 +21,7 @@ public class Device : NSObject {
         case TV                // = 3
         case CarPlay           // = 4
     }
-    
+
     /**
      The screen orientation of the device
      */
@@ -30,10 +30,10 @@ public class Device : NSObject {
         case Portrait          // = 1
         case Landscape         // = 2
     }
-    
+
     /**
      The physical type and screen orientation of the device
-     
+
      Math at its finest! The combination of both TYPE and ORIENTATION is the sum of the TYPE [in the tens place] and the ORIENTATION [in the ones place]
      */
     public enum TYPE_AND_ORIENTATION : Int {
@@ -53,7 +53,7 @@ public class Device : NSObject {
         case CarPlayPortrait      = 41
         case CarPlayLandscape     = 42
     }
-    
+
     /**
      The battery status of the device
      */
@@ -63,7 +63,7 @@ public class Device : NSObject {
         case Full
         case Unplugged
     }
-    
+
     /**
      The proximity to user of the device
      */
@@ -72,68 +72,68 @@ public class Device : NSObject {
         case CloseToUser
         case AwayFromUser
     }
-    
+
     /**
      The physical type of the device (read-only)
-     
+
      Possible values:
      .Phone / .Pad / .TV / .CarPlay / .Unspecified
      */
     public static var type: TYPE {
         get {
-            switch UIDevice.currentDevice().userInterfaceIdiom {
-            case .Phone:
+            switch UIDevice.current.userInterfaceIdiom {
+            case .phone:
                 return .Phone
-            case .Pad:
+            case .pad:
                 return .Pad
-            case .TV:
+            case .tv:
                 return .TV
-            case .CarPlay:
+            case .carPlay:
                 return .CarPlay
             default:
                 return .Unspecified
             }
         }
     }
-    
+
     /**
      The screen orientation of the device (read-only)
-     
+
      Possible values:
      .Portrait / .Landscape / .Unknown
      */
     public static var orientation: ORIENTATION {
         get {
-            switch UIDevice.currentDevice().orientation {
-            case .Portrait:
+            switch UIDevice.current.orientation {
+            case .portrait:
                 return .Portrait
-            case .PortraitUpsideDown:
+            case .portraitUpsideDown:
                 return .Portrait
-            case .LandscapeLeft:
+            case .landscapeLeft:
                 return .Landscape
-            case .LandscapeRight:
+            case .landscapeRight:
                 return .Landscape
             default:
                 return .Unknown
             }
         }
     }
-    
+
     /**
      The [fully Apple-specified] screen orientation of the device (read-only)
-     
+
      Possible values:
      .Portrait / .PortraitUpsideDown / .LandscapeLeft / .LandscapeRight / .FaceUp / .FaceDown / .Unknown
      */
     public static var orientationDetail: UIDeviceOrientation {
         get {
-            return UIDevice.currentDevice().orientation
+            return UIDevice.current.orientation
         }
     }
-    
+
     /**
      The physical type and screen orientation of the device (read-only)
-     
+
      Possible values:
      .UnspecifiedPortrait / .UnspecifiedLandscape / .UnspecifiedUnknown / .PhoneUnknown / .PhonePortrait / .PhoneLandscape / .PadUnknown / .PadPortrait / .PadLandscape / .TVUnknown / .TVPortrait / .TVLandscape / .CarPlayUnknown / .CarPlayPortrait / .CarPlayLandscape
      */
@@ -143,40 +143,40 @@ public class Device : NSObject {
             return TYPE_AND_ORIENTATION.init(rawValue: device)!
         }
     }
-    
+
     /**
      Retrieve, enable, and disable the current device's battery monitoring
-     
+
      The default system state is `battery monitoring disabled`. To set a new battery monitoring state: `Device.batteryMonitored = true` or `Device.batteryMonitored = false`
-     
+
      Possible values:
      true / false
      */
     public static var batteryMonitored: Bool {
         get {
-            return UIDevice.currentDevice().batteryMonitoringEnabled
+            return UIDevice.current.isBatteryMonitoringEnabled
         }
         set {
-            UIDevice.currentDevice().batteryMonitoringEnabled = !UIDevice.currentDevice().batteryMonitoringEnabled
+            UIDevice.current.isBatteryMonitoringEnabled = !UIDevice.current.isBatteryMonitoringEnabled
         }
     }
-    
+
     /**
      The battery charging status of the device (read-only)
-     
+
      - Note: battery monitoring must be enabled to retrieve the charging status. Use `Device.batteryMonitored` to retrieve the battery monitoring status, to enable it, or to disable it.
-     
+
      Possible values:
      .Charging / .Full / .Unplugged / .Unknown
      */
     public static var batteryStatus: BATTERY_STATUS {
         get {
-            switch UIDevice.currentDevice().batteryState {
-            case .Charging:
+            switch UIDevice.current.batteryState {
+            case .charging:
                 return .Charging
-            case .Full:
+            case .full:
                 return .Full
-            case .Unplugged:
+            case .unplugged:
                 return .Unplugged
             default:
                 // UIDevice.currentDevice().batteryMonitoringEnabled is 'false' OR state is truly unknown
@@ -184,55 +184,55 @@ public class Device : NSObject {
             }
         }
     }
-    
+
     /**
      The current battery level of the device (read-only)
-     
+
      - Note: battery monitoring must be enabled to retrieve the battery level. Use `Device.batteryMonitored` to retrieve the battery monitoring status, to enable it, or to disable it.
-     
+
      Possible values:
      [0.0] for empty to [1.0] for full (decimal percentage)
      [-1.0] for no value detected
      */
     public static var batteryLevel: Float {
         get {
-            if UIDevice.currentDevice().batteryMonitoringEnabled {
-                return UIDevice.currentDevice().batteryLevel
+            if UIDevice.current.isBatteryMonitoringEnabled {
+                return UIDevice.current.batteryLevel
             } else {
                 return -1.0
             }
         }
     }
-    
+
     /**
      Retrieve, enable, and disable the current device's proximity monitoring
-     
+
      The default system state is `proximity monitoring disabled`. To set a new proximity monitoring state: `Device.proximityMonitored = true` or `Device.proximityyMonitored = false`
-     
+
      Possible values:
      true / false
      */
     public static var proximityMonitored: Bool {
         get {
-            return UIDevice.currentDevice().proximityMonitoringEnabled
+            return UIDevice.current.isProximityMonitoringEnabled
         }
         set {
-            UIDevice.currentDevice().proximityMonitoringEnabled = !UIDevice.currentDevice().proximityMonitoringEnabled
+            UIDevice.current.isProximityMonitoringEnabled = !UIDevice.current.isProximityMonitoringEnabled
         }
     }
-    
+
     /**
      The proximity of the device to the user (read-only)
-     
+
      - Note: proximity monitoring must be enabled to retrieve the proximity of the device to the user. Use `Device.proximityMonitored` to retrieve the proximity monitoring status, to enable it, or to disable it.
-     
+
      Possible values:
      .CloseToUser / .AwayFromUser / .Unknown
      */
     public static var proximityToUser: PROXIMITY_TO_USER {
         get {
             if Device.proximityMonitored {
-                switch UIDevice.currentDevice().proximityState {
+                switch UIDevice.current.proximityState {
                 case true:
                     return .CloseToUser
                 case false:
@@ -243,58 +243,58 @@ public class Device : NSObject {
             }
         }
     }
-    
+
     /**
      The name of device (read-only)
-     
+
      Possible values:
      String containing the device name
      */
     public static var name: String {
         get {
-            return UIDevice.currentDevice().name
+            return UIDevice.current.name
         }
     }
-    
+
     /**
      The model of the device (read-only)
-     
+
      Possible values:
      String containing the device model
      */
     public static var model: String {
         get {
-            return UIDevice.currentDevice().model
+            return UIDevice.current.model
         }
     }
-    
+
     /**
      The name of the operating system running on the device (read-only)
-     
+
      Possible values:
      String containing the device operating system name
      */
     public static var osName: String {
         get {
-            return UIDevice.currentDevice().systemName
+            return UIDevice.current.systemName
         }
     }
-    
+
     /**
      The version of the operating system running on the device (read-only)
-     
+
      Possible values:
      String containing the device operating system version number
      */
     public static var osVersion: String {
         get {
-            return UIDevice.currentDevice().systemVersion
+            return UIDevice.current.systemVersion
         }
     }
-    
+
     /**
      The name and version of the operating system running on the device (read-only)
-     
+
      Possible values:
      String containing the device operating system name and version number
      */
@@ -303,29 +303,29 @@ public class Device : NSObject {
             return self.osName + " " + self.osVersion
         }
     }
-    
+
     /**
      The identification of the device for vendor (read-only)
-     
+
      - Note:
      Determinded by data provided by App Store (example: App Store ID), otherwise the app's bundle ID (example: com.example.app)
-     
+
      Possible values:
      Strinc containing the "identification for vendor" string
      */
     public static var idForVendor: String {
         get {
-            return (UIDevice.currentDevice().identifierForVendor?.UUIDString)!
+            return (UIDevice.current.identifierForVendor?.uuidString)!
         }
     }
-    
+
     /**
      The existence of GPS hardware on the device (read-only)
-     
+
      Possible values:
      [true] for devices containing GPS hardware
      [false] for devices NOT containing GPS hardware
-     
+
      - Note:
      The GPS capability of the device returned is based on the actual hardware identifier that Apple uses for its devices (example: iPad5,3); the device list was created with a combination of Apple's official [iOS Device Compatibility Reference](https://developer.apple.com/library/ios/documentation/DeviceInformation/Reference/iOSDeviceCompatibility/DeviceCompatibilityMatrix/DeviceCompatibilityMatrix.html) and hardware identifiers from [EveryMac.com](http://www.everymac.com/)
      */
@@ -353,10 +353,14 @@ public class Device : NSObject {
                 "iPad5,3",
                 "iPad6,3",
                 "iPad6,7",
+		"iPad6,11",
+		"iPad7,1",
+		"iPad7,3",
+		"iPad7,5"
                 ]
-            
+
             let hardware = self.hardwareIdentifier
-            
+
             if hardware != "Unknown" {
                 if nonGPSCapableDevices.contains(hardware) {
                     return false
@@ -368,34 +372,32 @@ public class Device : NSObject {
             }
         }
     }
-    
+
     /**
      The hardware identifier of the device [example: iPad5,3] (read-only)
-     
+
      Possible values:
      String containing device hardware identifier string
-     
+
      ***Credit:*** this code is a modified version of the DeviceGuru.swift function hardwareString() from [DeviceGuru](https://github.com/InderKumarRathore/DeviceGuru)
      */
     public static var hardwareIdentifier: String {
         get {
             var name: [Int32] = [CTL_HW, HW_MACHINE]
             var size: Int = 2
-            sysctl(&name, 2, nil, &size, &name, 0)
-            var hw_machine = [CChar](count: Int(size), repeatedValue: 0)
-            sysctl(&name, 2, &hw_machine, &size, &name, 0)
-            
-            if let hardware = String.fromCString(hw_machine) {
-                // check if device is a simulator
-                if hardware == "i386" || hardware == "x86_64" {
-                    return self.name
-                } else {
-                    return hardware
-                }
-            } else { // an error getting hardware identifier string occurred
-                return "Unknown"
+            var tname: [Int32] = name
+            sysctl(&tname, 2, nil, &size, &name, 0)
+            var hw_machine = [CChar](repeating: 0, count: Int(size))
+            sysctl(&tname, 2, &hw_machine, &size, &name, 0)
+
+            let hardware = String(cString: hw_machine)
+
+            // check if device is a simulator
+            if hardware == "i386" || hardware == "x86_64" {
+                return self.name
+            } else {
+                return hardware
             }
         }
     }
-    
 }
